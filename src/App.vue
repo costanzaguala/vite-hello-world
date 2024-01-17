04e90a8e27614beddb353657be79d25c2850a01f
costanzaguala <costanzasilviaguala@gmail.com>
2024-01-17 13:41:27 +0100

    esercizio finito

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+    import AppTitle from './components/AppTitle.vue';
+
+    export default {
+    data() {
+        return { count: 0 
+        }
+     },
+
+     components:{
+        AppTitle
+     }
+}
+</script>
+
+<template>
+    <div>
+        <AppTitle/>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        data() {
+            return {
+                title: 'La mia prima app con Vite!'
+            }
+        }
+    };
+</script>
+
+<template>
+    <header>
+        <h1 class="main-title">
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+    color: blue;
+    font-size: 16px;
+}
+</style>
